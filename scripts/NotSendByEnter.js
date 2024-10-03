aaa356081e208f6aa7a73afe73a7eba1bef38a6b
soranjiro <122503868+soranjiro@users.noreply.github.com>
2024-10-03 20:55:07 +0900

    Update NotSendByEnter.js

--- scripts/NotSendByEnter.js
+++ scripts/NotSendByEnter.js
@@ -2,22 +2,22 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log("running boost...");
 
-  // XPathで要素を取得する関数
-  function getElementByXPath(xpath) {
-    const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-    return result.singleNodeValue;
+  // クラス名で要素を取得する関数
+  function getElementByClassName(className) {
+    return document.querySelector(`.${className}`);
   }
 
-  // 質問するテキストエリアをXPathで指定（XPathはNotebooklm）
-  const xpath = '/html/body/labs-tailwind-root/div/notebook/div/div[2]/div/div/div[2]/chat-layout/div/omnibar/div/div[2]/div[2]/query-box/div/div/form/textarea';
+  // 質問するテキストエリアをクラス名で指定
+  const className = 'query-box-input';
+  let cmdEnter = false;
 
-  function handleKeydown(e, cmdEnter) {
+  function handleKeydown(e) {
     console.log("Keydown event detected:", e);
 
     // 押されたキーがCmd + Enterだった場合のみ
     if (e.key === 'Enter' && e.metaKey) {
       console.log("Cmd + Enter detected");
-      cmdEnter.value = true;
+      cmdEnter = true;
       // プログラム的に Enter キーの押下イベントを発生させる
       const enterEvent = new KeyboardEvent('keydown', {
         key: 'Enter',
@@ -28,12 +28,12 @@ document.addEventListener('DOMContentLoaded', () => {
         cancelable: true
       });
       e.target.dispatchEvent(enterEvent);
-      cmdEnter.value = false;
+      cmdEnter = false;
       return;
     }
 
     // それ以外の場合はイベントの伝播を中止する(日本語の確定は行われる)
-    if (e.key === 'Enter' && !cmdEnter.value) {
+    if (e.key === 'Enter' && !cmdEnter) {
       console.log("Stopping immediate propagation");
       e.preventDefault();
       e.stopImmediatePropagation();
@@ -41,17 +41,15 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function checkForTextarea() {
-    const textarea = getElementByXPath(xpath);
+    const textarea = getElementByClassName(className);
     if (textarea) {
       console.log("Textarea found:", textarea);
       console.log("Textarea content:", textarea.value);
 
-      const cmdEnter = { value: false };
-
       // そのエリア内でのキーダウン(キーの入力)のイベントを検知
-      textarea.addEventListener('keydown', (e) => handleKeydown(e, cmdEnter), true); // capture フェーズでイベントリスナーを設定
+      textarea.addEventListener('keydown', handleKeydown, true); // capture フェーズでイベントリスナーを設定
     } else {
-      console.error("Textarea not found. XPath:", xpath);
+      console.error("Textarea not found. ClassName:", className);
       setTimeout(checkForTextarea, 1000); // 1秒後に再チェック
     }
   }
